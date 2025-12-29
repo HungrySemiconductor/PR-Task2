199e4632fc6285b845a6f57517a5780529c027be
Fan <fhongle@163.com>
2025-12-29 21:44:11 +0800

    更新报告结构 + 完善数据预处理、参数初始化、前向传播

--- main.py
+++ main.py
@@ -4,14 +4,26 @@ from data_loader import prepare_data, split_data
 
 class NeuralNetwork():
     # 网络参数
-    def __init__(self, input_size, hidden_size, output_size, learning_rate=0.01):
-        pass
+    def __init__(self, input_size, hidden_size, output_size, learning_rate):
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.learning_rate = learning_rate
+        
+        # 初始化权重
+        self.init_weights()
 
-    # 权重初始化
+    # ==================== 权重初始化 ====================
     def init_weights(self):
-        pass
-    
-    # 激活函数及其求导
+        # 输入层到隐含层
+        self.W1 = np.random.randn(self.input_size, self.hidden_size) * 0.01
+        self.b1 = np.zeros((1, self.hidden_size))
+
+        # 隐含层到输出层
+        self.W2 = np.random.randn(self.hidden_size, self.output_size) * 0.01
+        self.b2 = np.zeros((1, self.output_size))
+
+    # ==================== 激活函数及其求导 ====================
     def sigmoid(self, x):
         return 1 / (1 + np.exp(-x))
     
@@ -19,30 +31,50 @@ class NeuralNetwork():
         sig = self.sigmoid(x)
         return sig * (1 - sig)
     
-    def tanh_derivation(self, x):
+    def tanh(self, x):
+        return np.tanh(x)
+    
+    def tanh_derivative(self, x):
         return 1.0 - np.tanh(x)**2
 
 
-    # 前向传播
+    # ==================== 前向传播 ====================
     def forward(self, X):
-        pass
+        # 输入层到隐含层
+        self.net_h = np.dot(X, self.W1) + self.b1  
+        self.y_h  = self.tanh(self.net_h)          
+        
+        # 隐含层到输出层
+        self.net_j = np.dot(self.y_h , self.W2) + self.b2   
+        self.z_j = self.sigmoid(self.net_j)    
 
-    # 损失函数计算
+        return self.z_j 
+
+
+    # ==================== 损失函数计算 MSE ====================
     def compute_loss(self, y_true, y_pred):
-        pass
+        batch_size = y_true.shape[0]
+        loss = 0.5 * np.sum((y_true - y_pred) ** 2) / batch_size
+        return loss
+
+
+    # ==================== 反向传播 ====================
 
     # 单样本更新
-    def train_signle_sample(self, X, y, verbose=False):
+    def train_single_sample(self, X, y, verbose=False):
         pass
 
     # 批量更新
     def train_batch(self, X_batch, y_batch, verbose=False):
         pass
 
-    # 训练
+
+
+    # ==================== 训练循环 ====================
     def train(self, X_train, y_train, X_val, y_val, epochs=1000, batch_type='batch', verbose=100):
         pass
 
+    # ==================== 预测评估 ====================
     # 预测
     def predict(self, X):
         pass
